3df4421b70372c65c5366b1601a0e423c75859e6
41223234 <144989696+41223234@users.noreply.github.com>
2023-12-20 19:37:52 +0000

    又10test

---
+++ C.EX5.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/* Example of using a switch on characters */
+int main()
+{
+ char this_is_a_character;
+ printf("Please enter character a,b,c,d or e:\n ");
+ scanf("%c", &this_is_a_character);
+ switch (this_is_a_character)
+ {
+ case 'a':
+ printf("a entered");
+ break;
+ case 'b':
+ printf("b entered");
+ break;
+Chapter 1 Introduction to C
+16
+ case 'c':
+ printf("c entered");
+ break;
+ case 'd':
+ printf("d entered");
+ break;
+ case 'e':
+ printf("e entered");
+ break;
+ default:
+ printf("Default ");
+ }
+ return 0;
+}
